fe72a371f9ce188eae8e5a35a5046dc09a1014a3
chris.e@gathi.com <chris.e@gathi.com>
2022-04-26 17:50:14 +0000

    -

---
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -0,0 +1,14 @@
+with orders as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from raw.jaffle_shop.orders
+
+)
+
+{{ limit_data_in_dev(column_name = 'order_date', dev_days_of_data = 1000) }}
+
